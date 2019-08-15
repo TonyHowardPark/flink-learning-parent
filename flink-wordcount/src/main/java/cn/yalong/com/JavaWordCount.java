fe964518e4088f0a60e0696a758921634d715c0e
zhouyalong <zhouyalong@nifa.org.cn>
2019-08-15 10:39:43 +0800

    from file

--- flink-wordcount/src/main/java/cn/yalong/com/JavaWordCount.java
+++ flink-wordcount/src/main/java/cn/yalong/com/JavaWordCount.java
@@ -1,23 +1,47 @@
 package cn.yalong.com;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.*;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 
 public class JavaWordCount {
     public static void main(String[] args) throws Exception {
+
+        //Creates an execution environment
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
+        // read file from file
         DataSet<String> text = env.readTextFile(".\\data\\student.txt");
 
-        DataSet<Tuple2<String, Integer>> counts =
-                text.flatMap(new Tokenizer()).groupBy(0).sum(1);
+        //split
+        DataSet<Tuple2<String, Integer>> counts =text.flatMap(new Tokenizer()).groupBy(0).sum(1);
 
-        counts.print();
+    /*    //split
+        FlatMapOperator<String, String> words = text.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String value, Collector<String> out) throws Exception {
+                String[] result = value.split(" ");
+            }
+        });
+
+        MapOperator<String, Tuple2<String, Integer>> wordAnd1 = words.map(new MapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> map(String value) throws Exception {
+                return new Tuple2<>(value, 1);
+            }
+        });
+
+        UnsortedGrouping<Tuple2<String, Integer>> reduceByKey = wordAnd1.groupBy(0);
+
+        AggregateOperator<Tuple2<String, Integer>> sum = reduceByKey.sum(1);
+
+        System.out.println(sum.collect());*/
     }
-
 
     public static class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
